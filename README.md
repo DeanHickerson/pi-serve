f1fdc56c3ec92e3bf705443ec83c858675be8b80
Dean H Commits <deanhickerson@gmail.com>
2020-07-25 10:20:47 -0400

    Added routes for API GET and POST

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pi-serve
 RPI Server and site
+
+Just a foundation for spinning up a server on my local Raspberry Pi.
--- index.js
+++ index.js
@@ -9,10 +9,26 @@ const app = express();
 
 app.use(cors());
 app.use(express.static('public'));
+app.use(express.json());
 
-// app.get('/', (req,res) => {
-// 	res.send();
-// });
+app.post('/api', (req,res) => {
+	console.log(req.body);
+	req.body.serve = 'we got it!';
+	res.send(req.body);
+});
+
+// send something like /api?name=dean
+app.get('/api', (req,res) => {
+	if(req.query) {
+		console.log(req.query);
+		if(req.query.name) {
+			res.send(req.query.name);
+			return
+		}
+	}
+	console.log('We got a request!');
+	res.send({response:'No data available yet'});
+});
 
 app.use(function (req, res, next) {
 	res.status(404).send("404 - Sorry can't find that!🙅🏻");

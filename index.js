a412b04820862013e7c00c40292f13a1f88a935c
Dean Hickerson <deanhickerson@gmail.com>
2021-06-12 12:23:04 -0400

    Added comments and updated api responses

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const app = express();
 
 // cors() will be set to allow "*" by default
 app.use(cors());
+
+// Use express static to serve static files from the public directory
 app.use(express.static('public'));
-app.use(express.json());
+
 // express.json() will explicitly require POST requests to have headers set
 // with 'Content-Type':'application/json' and the body will need to be in JSON format
 // example: fetch('localhost',{method:'POST',headers:{'Content-Type':'application/json'}})
+app.use(express.json());
 
 // send something like: fetch('localhost',{method:'POST',headers:{'Content-Type':'application/json'},body:{msg:'my message'}})
 app.post('/api', (req,res) => {
@@ -25,19 +28,19 @@ app.post('/api', (req,res) => {
 	res.send(req.body);
 });
 
-// send something like /api?name=dean
+// Send something like /api?name=dean
 // Leverage some validation/sanitization with check('*').esacpe()
 // The wildcard means we want to check (sanitize) ALL query params
 app.get('/api', check('*').escape().trim(), (req,res) => {
 	if(req.query) {
 		console.log(req.query);
 		if(req.query.name) {
-			res.send(req.query.name);
-			return
+			res.send(`Hi ${req.query.name}`);
+			return;
 		}
 	}
 	console.log('We got a request!');
-	res.send({response:'No data available yet'});
+	res.send({response:'You hit my API. Try a query next.'});
 });
 
 app.use(function (req, res, next) {
@@ -45,5 +48,5 @@ app.use(function (req, res, next) {
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is running at http://${os.hostname()}:${PORT}`)
+	console.log(`Server is running at http://${os.hostname()}:${PORT}`);
 });
